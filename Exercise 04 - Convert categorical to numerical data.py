14731464bf09bc48258c5b8851c5f94105810fc1
datamesse <48945612+datamesse@users.noreply.github.com>
2022-03-12 19:09:19 +1100

    Update Exercise 04 - Convert categorical to numerical data.py

--- Exercise 04 - Convert categorical to numerical data.py
+++ Exercise 04 - Convert categorical to numerical data.py
@@ -4,8 +4,8 @@ Converting categorical to numerical data
 
 """
 Categorical encoding to numbers
-  Label encoding   - 2 distinct values (a.k.a binomial e.g. "Yes" = 1, "No" = 0)
-  One hot encoding - 3 or more distinct values (the values become columns and those columns have binomial treatment e.g. Column colour's values are blue, red, and yellow. Those are each made into a column with 0 or 1 as theeir vvalues)
+  One hot encoding - 2 distinct values (a.k.a binomial e.g. "Yes" = 1, "No" = 0)
+  Label encoding - 3 or more distinct values (the values become columns and those columns have binomial treatment e.g. Column colour's values are blue, red, and yellow. Those are each made into a column with 0 or 1 as theeir vvalues)
 
          Label encoding    One hot encoding
 -------------------------------------------
@@ -15,7 +15,6 @@ pandas   factorize( )      get_dummies
 Note: In live, there may be a different number of unique values for a field compared to the testing dataset.
 In which case, we fit the encoder function only to the training data.
 """
-
 
 import pandas as pd
 import numpy as np
@@ -32,7 +31,7 @@ data = pd.read_csv("https://raw.githubusercontent.com/datamesse/tutorial-linkedi
 
 
 """
-Option 1. Map each category to a differet function integer i.e. label encoding using Pandas
+Option 1. Label encoding using Pandas
 
 https://pandas.pydata.org/docs/reference/api/pandas.factorize.html
 .factorize() creates an array substituting each categorical value as an integer
@@ -54,6 +53,9 @@ https://www.w3schools.com/python/ref_func_dict.asp
 .dict() creates a dictionary,  which stores data values in key:value pairs
 """
 
+
+# import pandas as pd
+
 # region = data["region"] # retrieves the entire region column by itself
 # region_encoded, region_categories = pd.factorize(region)
 # factor_region_mapping = dict(zip(region_categories, region_encoded))
@@ -64,4 +66,60 @@ https://www.w3schools.com/python/ref_func_dict.asp
 # print(region_categories) # list of unique category values
 # print(region_encoded[:10]) # first 10 encoded numbers for categories
 # print(factor_region_mapping) # dictionary mapping of numbers with categorical values
+
+
+"""
+Option 2. One hot encoding using Pandas
+https://pandas.pydata.org/docs/reference/api/pandas.get_dummies.html
+.get_dummies can result in this
+  _northeast  _northwest  _southeast  _southwest
+0           0           0           0           1
+1           0           0           1           0
+2           0           0           1           0
+3           0           1           0           0
+"""
+
+# region = data["region"] # retrieves the entire region column by itself
+# region_encoded = pd.get_dummies(region, prefix='')
+
+# print("Pandas get_dummies function for one hot encoding with series")  
+# print(region_encoded) #encoded numbers for categories 
+
+
+"""
+Option 1. Label encoding using sklearn
+
+ndarray is a (usually fixed-size) multi-dimensional container of items of the same type and size
+
+"""
+
+#create ndarray for label encodoing (sklearn)
+sex = data.iloc[:,1:2].values
+smoker = data.iloc[:,4:5].values
+
+#label encoder = le
+
+
+## le for sex
+le = LabelEncoder()
+sex[:,0] = le.fit_transform(sex[:,0])
+sex = pd.DataFrame(sex)
+sex.columns = ['sex']
+le_sex_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("Sklearn label encoder results for sex:")
+print(le_sex_mapping)
+print(sex[:10])
+
+## le for smoker
+le = LabelEncoder()
+smoker[:,0] = le.fit_transform(smoker[:,0])
+smoker = pd.DataFrame(smoker)
+smoker.columns = ['smoker']
+le_smoker_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("Sklearn label encoder results for smoker:")
+print(le_smoker_mapping)
+print(smoker[:10])
+
+
+
 
