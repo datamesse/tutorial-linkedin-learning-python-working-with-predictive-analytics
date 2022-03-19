8ae6c49680eb18ed8bb22ee4bb9adb33aec78683
datamesse <48945612+datamesse@users.noreply.github.com>
2022-03-19 11:57:35 +1100

    Update Exercise 04 - Convert categorical to numerical data.py

--- Exercise 04 - Convert categorical to numerical data.py
+++ Exercise 04 - Convert categorical to numerical data.py
@@ -16,6 +16,7 @@ Note: In live, there may be a different number of unique values for a field comp
 In which case, we fit the encoder function only to the training data.
 """
 
+"""
 import pandas as pd
 import numpy as np
 
@@ -28,6 +29,10 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv("https://raw.githubusercontent.com/datamesse/tutorial-linkedin-learning-python-working-with-predictive-analytics/main/data/insurance.csv")
+"""
+
+
+
 
 
 """
@@ -53,19 +58,23 @@ https://www.w3schools.com/python/ref_func_dict.asp
 .dict() creates a dictionary,  which stores data values in key:value pairs
 """
 
+"""
+import pandas as pd
+data = pd.read_csv("https://raw.githubusercontent.com/datamesse/tutorial-linkedin-learning-python-working-with-predictive-analytics/main/data/insurance.csv")
 
-# import pandas as pd
+region = data["region"] # retrieves the entire region column by itself
+region_encoded, region_categories = pd.factorize(region)
+factor_region_mapping = dict(zip(region_categories, region_encoded))
 
-# region = data["region"] # retrieves the entire region column by itself
-# region_encoded, region_categories = pd.factorize(region)
-# factor_region_mapping = dict(zip(region_categories, region_encoded))
+print("Pandas factorize function for label encoding with series")  
+print(region[:10]) # first 10 rows of original data 
+print(region_categories) # list of unique category values
+print(region_encoded[:10]) # first 10 encoded numbers for categories
+print(factor_region_mapping) # dictionary key:value mapping of numbers with categorical values
+"""
 
 
-# print("Pandas factorize function for label encoding with series")  
-# print(region[:10]) # first 10 rows of original data 
-# print(region_categories) # list of unique category values
-# print(region_encoded[:10]) # first 10 encoded numbers for categories
-# print(factor_region_mapping) # dictionary mapping of numbers with categorical values
+
 
 
 """
@@ -79,20 +88,30 @@ https://pandas.pydata.org/docs/reference/api/pandas.get_dummies.html
 3           0           1           0           0
 """
 
-# region = data["region"] # retrieves the entire region column by itself
-# region_encoded = pd.get_dummies(region, prefix='')
 
-# print("Pandas get_dummies function for one hot encoding with series")  
-# print(region_encoded) #encoded numbers for categories 
+import pandas as pd
+data = pd.read_csv("https://raw.githubusercontent.com/datamesse/tutorial-linkedin-learning-python-working-with-predictive-analytics/main/data/insurance.csv")
+
+region = data["region"] # retrieves the entire region column by itself
+region_encoded = pd.get_dummies(region, prefix='')
+
+print("Pandas get_dummies function for one hot encoding with series")  
+print(region_encoded) #encoded numbers for categories 
+
+
+
+
 
 
 """
-Option 1. Label encoding using sklearn
+Option 3. Label encoding using sklearn
 
 ndarray is a (usually fixed-size) multi-dimensional container of items of the same type and size
 
 """
 
+
+"""
 #create ndarray for label encodoing (sklearn)
 gender = data.iloc[:,1:2].values
 smoker = data.iloc[:,4:5].values
@@ -104,7 +123,7 @@ gender[:,0] = le.fit_transform(gender[:,0])
 gender = pd.DataFrame(gender)
 gender.columns = ['sex']
 le_gender_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
-print("Sklearn label encoder results for sex:")
+print("Sklearn label encoder results for gender:")
 print(le_gender_mapping)
 print(gender[:10])
 
@@ -121,3 +140,4 @@ print(smoker[:10])
 
 
 
+"""
