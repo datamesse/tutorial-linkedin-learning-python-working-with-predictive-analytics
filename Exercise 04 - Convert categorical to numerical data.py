2bf13665238d533d901f9989e13dfe3b6d397925
datamesse <48945612+datamesse@users.noreply.github.com>
2022-03-22 20:39:55 +1100

    Update Exercise 04 - Convert categorical to numerical data.py

--- Exercise 04 - Convert categorical to numerical data.py
+++ Exercise 04 - Convert categorical to numerical data.py
@@ -88,7 +88,7 @@ https://pandas.pydata.org/docs/reference/api/pandas.get_dummies.html
 3           0           1           0           0
 """
 
-
+"""
 import pandas as pd
 data = pd.read_csv("https://raw.githubusercontent.com/datamesse/tutorial-linkedin-learning-python-working-with-predictive-analytics/main/data/insurance.csv")
 
@@ -97,7 +97,7 @@ region_encoded = pd.get_dummies(region, prefix='')
 
 print("Pandas get_dummies function for one hot encoding with series")  
 print(region_encoded) #encoded numbers for categories 
-
+"""
 
 
 
@@ -105,18 +105,27 @@ print(region_encoded) #encoded numbers for categories
 
 """
 Option 3. Label encoding using sklearn
+Conceptually, an ndarray is a (usually fixed-size) multi-dimensional container of items of the same type and size
+https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iloc.html
+.iloc[] 
 
-ndarray is a (usually fixed-size) multi-dimensional container of items of the same type and size
 
 """
 
 
-"""
+#"""
+import pandas as pd
+data = pd.read_csv("https://raw.githubusercontent.com/datamesse/tutorial-linkedin-learning-python-working-with-predictive-analytics/main/data/insurance.csv")
+
+from sklearn.preprocessing import LabelEncoder
+
 #create ndarray for label encodoing (sklearn)
 gender = data.iloc[:,1:2].values
 smoker = data.iloc[:,4:5].values
 
+print(gender)
 
+"""
 ## le for gender
 le = LabelEncoder()
 gender[:,0] = le.fit_transform(gender[:,0])
@@ -126,6 +135,7 @@ le_gender_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
 print("Sklearn label encoder results for gender:")
 print(le_gender_mapping)
 print(gender[:10])
+
 
 ## le for smoker
 le = LabelEncoder()
@@ -140,4 +150,4 @@ print(smoker[:10])
 
 
 
-"""
+#"""
